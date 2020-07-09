dad27c4629354ea435452f895bad9a6aedef35e8
LSVG <laura2162144@correo.uis.edu.co>
2020-07-08 23:05:49 -0500

    Listo punto 3

--- validacion.js
+++ validacion.js
@@ -3,7 +3,9 @@ var apellido = document.getElementById('apellido');
 var direccion = document.getElementById('direccion');
 var ccusuario = document.getElementById('ccusuario');
 var ccpaswd = document.getElementById('ccpaswd');
+var ccpaswdValidate = document.getElementById('ccpaswdValidate');
 var email = document.getElementById('email');
+var error = document.getElementById('error')
 
 function enviarFormulario(){
 	var mensajesError = [];
@@ -11,6 +13,38 @@ function enviarFormulario(){
 	if(nombre.value === null || nombre.value === ''){
 		mensajesError.push('Ingrese el nombre');
 	}
+	
+	if(nombre.value.length > 25){
+		mensajesError.push('El nombre no debe exceder los 25 caracteres');
+	}
+	if(apellido.value === null || apellido.value === ''){
+		mensajesError.push('Ingrese el Apellido');
+	}
+
+	if(apellido.value.length > 25){
+		mensajesError.push('El apellido no debe exceder los 25 caracteres');
+	}
+	if(email.value === null || email.value === ''){
+		mensajesError.push('Ingrese el Email');
+	}
+
+	if(email.value.length > 120){
+		mensajesError.push('El email no debe exceder los 120 caracteres');
+	}
+
+	direccion = direccion.value
+	direccion = direccion.toLowerCase();
+	var val =0;
+
+	if(direccion.startsWith('cll') || direccion.startsWith('cra') || direccion.startsWith('av') || direccion.startsWith('anv') || direccion.startsWith('trans')){
+		val = 1;
+	}
+
+	if(val === 0){
+		mensajesError.push('Direccion no valida, esta debe comenzar en Cll, Cra, Av, Anv, o Trans')
+	}
+
+	error.innerHTML = mensajesError.join(', ')
 
 	return false;
 }
