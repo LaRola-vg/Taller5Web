88c29e7ccbfc9e188f940a1221a2e47dba16aed5
LSVG <laura2162144@correo.uis.edu.co>
2020-07-08 20:21:33 -0500

    prueba

--- validacion.js
+++ validacion.js
@@ -1,1 +1,16 @@
-var nombre= document.getElementById('')
+var nombre = document.getElementById('nombre');
+var apellido = document.getElementById('apellido');
+var direccion = document.getElementById('direccion');
+var ccusuario = document.getElementById('ccusuario');
+var ccpaswd = document.getElementById('ccpaswd');
+var email = document.getElementById('email');
+
+function enviarFormulario(){
+	var mensajesError = [];
+
+	if(nombre.value === null || nombre.value === ''){
+		mensajesError.push('Ingrese el nombre');
+	}
+
+	return false;
+}
