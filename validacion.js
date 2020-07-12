6d1c8c3fbd771ae73d5f88db040a35ff2cd32cf0
LSVG <laura2162144@correo.uis.edu.co>
2020-07-11 22:11:18 -0500

    listas todas las validaciones

--- validacion.js
+++ validacion.js
@@ -44,6 +44,88 @@ function enviarFormulario(){
 		mensajesError.push('Direccion no valida, esta debe comenzar en Cll, Cra, Av, Anv, o Trans')
 	}
 
+	var ccusrval = 0;
+
+	if(ccusuario.value === null || ccusuario.value === ''){
+		mensajesError.push('Ingrese el campo ccusuario');
+	}
+	else{
+		if (ccusuario.value.length > 20) {
+			mensajesError.push('El campo ccusuario no debe exceder los 20 caracteres');
+		}else{
+			if(ccusuario.value.length < 10){
+				mensajesError.push('El campo ccusuario debe tener por lo menos 10 carcteres');
+			}else{
+				for(var i=0; i<ccusuario.value.length; i++){
+					var x = ccusuario.value.charCodeAt(i)
+					if(x < 65 || x > 90){
+						if(x<97 || x>122){
+							if(x<48 || x>57){
+								ccusrval = 1;
+							}
+						}
+					}
+				}
+				if(ccusrval==1){
+					mensajesError.push('El campo ccusuario solo debe contener lestras o numeros')
+				}
+			}
+		}
+	}
+
+	var ccpaswdval =0;
+	var ccpaswdcontMayus =0; //contador de letras mayusculas de la contraseña
+	var ccpaswdcontMinus =0; //contador de letras minusculas de la contraseña
+	var ccpaswdcontNum =0; //contador de numeros de la contraseña
+
+	if(ccpaswd.value === null || ccpaswd.value === ''){
+		mensajesError.push('Ingrese la contraseña');
+	}
+	else{
+		if (ccpaswd.value.length > 20) {
+			mensajesError.push('La contraseña no debe exceder los 20 caracteres');
+		}else{
+			if(ccpaswd.value.length < 15){
+				mensajesError.push('La contraseña debe tener por lo menos 10 carcteres');
+			}else{
+				for(var i=0; i<ccpaswd.value.length; i++){
+					var x = ccpaswd.value.charCodeAt(i)//AQUI SE OBTIENE EL VALOR EN CODIGO ASCCI DE CADA CARACTER
+					if(x < 65 || x > 90){
+						if(x<97 || x>122){
+							if(x<48 || x>57){
+								if(x == 35 || x == 37 || x == 47 || x == 38){
+									ccpaswdval = 0;
+								}
+								else{
+									ccpaswdval = 1;
+								}
+							}
+						}
+					}
+					if(x >= 65 && x <= 90){
+						ccpaswdcontMayus=ccpaswdcontMayus+1;
+					}
+					if(x >= 97 && x <= 122){
+						ccpaswdcontMinus=ccpaswdcontMinus+1;
+					}
+					if(x>=48 || x<=57){
+						ccpaswdcontNum=ccpaswdcontNum+1;
+					}
+				}
+				if(ccpaswdval==1){
+					mensajesError.push('La contraseña solo debe contener lestras, numeros o los caracteres: #, /, &, %')
+				}
+				if(ccpaswdcontNum==0 || ccpaswdcontMayus==0 || ccpaswdcontMinus==0){
+					mensajesError.push('La contraseña debe contener al menos una letra mayuscula, una minuscula y un numero (opcionalmente tambien los caracteres: #, /, &, %)')
+				}
+			}
+		}
+	}
+
+	if(ccpaswd.value != ccpaswdValidate.value){
+		mensajesError.push('Las contraseñas no coinciden')
+	}
+
 	error.innerHTML = mensajesError.join(', ')
 
 	return false;
